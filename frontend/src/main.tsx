fa52653e5e84d2611eb3af6c786abd1739dae99c
mrpythen <herman.nysted@gmail.com>
2026-05-01 11:16:00 +0200

    Add randomUUID fallback for HTTP frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,6 +19,7 @@
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./services/crypto/randomUuidFallback";
 import "./index.css";
 import App from "./App";
 import { ToastProvider } from "./contexts/Toast/ToastContext";
